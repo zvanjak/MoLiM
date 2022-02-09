5d978e126d7ce6f0868de7989553ba23988f8dcb
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-02-09 15:38:10 +0100

    slabo ide

--- scrap2.py
+++ scrap2.py
@@ -1,76 +1,49 @@
-import sys
+# import both Requests and Beautifulsoup
+
 import requests
-import lxml.html
 
-def main(id):
-    hxs = lxml.html.document_fromstring(requests.get("http://www.imdb.com/title/" + id).content)
-    movie = {}
-    try:
-        movie['title'] = hxs.xpath('//*[@id="overview-top"]/h1/span[1]/text()')[0].strip()
-    except IndexError:
-        movie['title']
-    try:
-        movie['year'] = hxs.xpath('//*[@id="overview-top"]/h1/span[2]/a/text()')[0].strip()
-    except IndexError:
-        try:
-            movie['year'] = hxs.xpath('//*[@id="overview-top"]/h1/span[3]/a/text()')[0].strip()
-        except IndexError:
-            movie['year'] = ""
-    try:
-        movie['certification'] = hxs.xpath('//*[@id="overview-top"]/div[2]/span[1]/@title')[0].strip()
-    except IndexError:
-        movie['certification'] = ""
-    try:
-        movie['running_time'] = hxs.xpath('//*[@id="overview-top"]/div[2]/time/text()')[0].strip()
-    except IndexError:
-        movie['running_time'] = ""
-    try:
-        movie['genre'] = hxs.xpath('//*[@id="overview-top"]/div[2]/a/span/text()')
-    except IndexError:
-        movie['genre'] = []
-    try:
-        movie['release_date'] = hxs.xpath('//*[@id="overview-top"]/div[2]/span[3]/a/text()')[0].strip()
-    except IndexError:
-        try:
-            movie['release_date'] = hxs.xpath('//*[@id="overview-top"]/div[2]/span[4]/a/text()')[0].strip()
-        except Exception:
-            movie['release_date'] = ""
-    try:
-        movie['rating'] = hxs.xpath('//*[@id="overview-top"]/div[3]/div[3]/strong/span/text()')[0]
-    except IndexError:
-        movie['rating'] = ""
-    try:
-        movie['metascore'] = hxs.xpath('//*[@id="overview-top"]/div[3]/div[3]/a[2]/text()')[0].strip().split('/')[0]
-    except IndexError:
-        movie['metascore'] = 0
-    try:
-        movie['description'] = hxs.xpath('//*[@id="overview-top"]/p[2]/text()')[0].strip()
-    except IndexError:
-        movie['description'] = ""
-    try:
-        movie['director'] = hxs.xpath('//*[@id="overview-top"]/div[4]/a/span/text()')[0].strip()
-    except IndexError:
-        movie['director'] = ""
-    try:
-        movie['stars'] = hxs.xpath('//*[@id="overview-top"]/div[6]/a/span/text()')
-    except IndexError:
-        movie['stars'] = ""
-    try:
-        movie['poster'] = hxs.xpath('//*[@id="img_primary"]/div/a/img/@src')[0]
-    except IndexError:
-        movie['poster'] = ""
-    try:
-        movie['gallery'] = hxs.xpath('//*[@id="combined-photos"]/div/a/img/@src')
-    except IndexError:
-        movie['gallery'] = ""
-    try:
-        movie['storyline'] = hxs.xpath('//*[@id="titleStoryLine"]/div[1]/p/text()')[0].strip()
-    except IndexError:
-        movie['storyline'] = ""
-    try:
-        movie['votes'] = hxs.xpath('//*[@id="overview-top"]/div[3]/div[3]/a[1]/span/text()')[0].strip()
-    except IndexError:
-        movie['votes'] = ""
-    return movie
+from bs4 import BeautifulSoup
 
-print (main("tt1905041"))
+
+class IMDBScraper:
+
+
+   def __init__(self, url):
+
+       self.url = url
+
+       self.download_page()
+
+
+
+   def download_page(self):
+
+       # method for downloading the hotel page
+
+       self.page = requests.get(self.url).text
+   
+   def scrape_data(self):
+
+       #method for scraping out movie title and description
+
+       soup = BeautifulSoup(self.page, "html.parser")
+
+       movie_title = soup.find("h1", {"data-testid": "hero-title-block__title"}).text
+
+       movie_description = soup.find("span", {"data-testid": "plot-xl"}).text
+
+       return {"title": movie_title,
+
+               "description": movie_description,
+
+               }
+
+
+
+urls = ["https://www.imdb.com/title/tt2382320/?ref_=hm_fanfav_tt_i_3_pd_fp1",]
+
+for url in urls:
+
+   x = IMDBScraper(url)
+
+   print(x.scrape_data())
