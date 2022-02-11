71dd982727d0f70d5c9c4d1f3c8297c64e9bb8f5
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-02-11 21:20:56 +0100

    malo

--- scrap2.py
+++ scrap2.py
@@ -1,49 +1,23 @@
-# import both Requests and Beautifulsoup
-
-import requests
-
-from bs4 import BeautifulSoup
+from imdb import Cinemagoer
 
 
-class IMDBScraper:
+# create an instance of the Cinemagoer class
+ia = Cinemagoer()
 
+# get a movie
+movie = ia.get_movie('0133093')
 
-   def __init__(self, url):
+# print the names of the directors of the movie
+print('Directors:')
+for director in movie['directors']:
+    print(director['name'])
 
-       self.url = url
+# print the genres of the movie
+print('Genres:')
+for genre in movie['genres']:
+    print(genre)
 
-       self.download_page()
-
-
-
-   def download_page(self):
-
-       # method for downloading the hotel page
-
-       self.page = requests.get(self.url).text
-   
-   def scrape_data(self):
-
-       #method for scraping out movie title and description
-
-       soup = BeautifulSoup(self.page, "html.parser")
-
-       movie_title = soup.find("h1", {"data-testid": "hero-title-block__title"}).text
-
-       movie_description = soup.find("span", {"data-testid": "plot-xl"}).text
-
-       return {"title": movie_title,
-
-               "description": movie_description,
-
-               }
-
-
-
-urls = ["https://www.imdb.com/title/tt2382320/?ref_=hm_fanfav_tt_i_3_pd_fp1",]
-
-for url in urls:
-
-   x = IMDBScraper(url)
-
-   print(x.scrape_data())
+# search for a person name
+people = ia.search_person('Mel Gibson')
+for person in people:
+   print(person.personID, person['name'])
