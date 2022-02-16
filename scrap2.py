b39b5e3fa0cac11f32e00c60c579cdd3ac0f3fe3
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-02-16 20:48:08 +0100

    poboljšanje

--- scrap2.py
+++ scrap2.py
@@ -9,9 +9,9 @@ ia = Cinemagoer()
 #   i godinu dohvatiti, za selekciju ako ima više filmova
 #  npr. testirati s Love, 
 #folder = "D:\Downloads"
-folder = "D:\Downloads\_Problematic"
+#folder = "D:\Downloads\_Problematic"
 #folder = "F:\FILMOVI\___1980's"
-#folder = "F:\\FILMOVI\\Novi_filmovi"
+folder = "F:\\FILMOVI\\Novi_filmovi"
 
 subfolders = [ f.name for f in os.scandir(folder) if f.is_dir() ]
 
@@ -19,7 +19,7 @@ fileErrors = open(folder + "\\FileErrors.txt",'w', encoding="utf-8")
 
 for movieName in subfolders:
   # provjeriti da li ima točku, ako nema ne diramo to -> provjerimo da li unutra ima nekih filmova!
-  if movieName.find('.') == -1:
+  if movieName.count('.') < 2:
     if movieName.find("IMDB"):
       print("\nDONE: " + movieName)
       continue
@@ -30,11 +30,17 @@ for movieName in subfolders:
     #print("\nSKIPPING: " + movieName)
     #fileErrors.write("\nSKIPPING - " + str(realMovieName) + " nema točke :)))  " + "\n\n")
 
-  else
+  else:
     parts = movieName.split('.')
 
   # naći prvi string koji je kredibilna godina proizvodnje (1930 - 2022)
+  cntParts=0
   for part in parts:
+    cntParts += 1
+    # prvoga bi trebalo preskočiti
+    if cntParts == 1:
+      continue
+    
     if( part.isnumeric() ):
       year = int(part)
       if year > 1930 and year < 2023 :
