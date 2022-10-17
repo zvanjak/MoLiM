fa603ff557076461f91055813564414e579d7852
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2022-10-17 19:47:49 +0200

    eee

--- scrap2.py
+++ scrap2.py
@@ -1101,7 +1101,8 @@ def rootFolderReportNoIMDBData(rootFolderName):
 def printMoviesList(listMovies):
   for movie in listMovies:
     short_cast = movie.cast_complete[0:75]
-    print("Rating - {0} - {1:55} - {2}    -   {3:20}   - {4}".format( movie.rating, movie.name + " (" + str(movie.year) + ") ", short_cast, movie.directors, movie.genres ) )
+    short_dir = movie.directors[0:20]
+    print("Rating - {0} - {1:55} - {2:75}  -  {3:20}  - {4}".format( movie.rating, movie.name + " (" + str(movie.year) + ") ", short_cast, short_dir, movie.genres ) )
 
 def printBigFiles():
   # The min size of the file in Bytes
